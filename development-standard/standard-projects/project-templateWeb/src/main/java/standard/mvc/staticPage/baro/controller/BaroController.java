308570217d5b88054dd465cce500c4eda5847b39
cyanism <6799635+cyanism@users.noreply.github.com>
2014-07-06 06:43:49 +0000

    바로보드란? 정적 페이지

---
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/baro/controller/BaroController.java
@@ -0,0 +1,62 @@
+package standard.mvc.staticPage.baro.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import standard.mvc.controller.GenericAbstractController;
+import standard.mvc.controller.GenericInterfaceController;
+import standard.mvc.dao.hibernate.SearchSupport;
+
+/**
+ * 바로보드 소개 > 바로보드란? 메뉴에 해당하는 컨트롤러
+ * @author 류강하
+ * @since 2014.06.30
+ */
+@Controller
+@RequestMapping(value = "/community/introduce/baro")
+public class BaroController extends GenericAbstractController implements GenericInterfaceController<Object> {
+
+	@Override
+	public Map<String, Map<String, Object>> bindTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@RequestMapping(value = "/select.do", method = RequestMethod.POST)
+	public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, Object parameterBean) {
+		
+		String viewResolver = "/thymeleaf";
+		String site = "/community";
+		String menu = "/staticPage/baro";
+		String target = "/baro";
+		
+		return viewResolver + site + menu + target;
+	}
+
+	@Override
+	public String invokeInsert(SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeUpdate(SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeDelete(SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
