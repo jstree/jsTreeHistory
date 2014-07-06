b20e77d41ff1eeb7371e1521ae694f0fbab77a9f
spear79 <spear79@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-06 13:22:30 +0000

    패키지 변경 Componenet Based Development

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/mybatis/MybatisSample.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/mybatis/MybatisSample.java
@@ -1,6 +1,6 @@
 package standard.mvc.component.base.dao.mybatis;
 
-import standard.mvc.dao.GenericInterfaceDao;
+import standard.mvc.component.base.dao.GenericInterfaceDao;
 
 public class MybatisSample implements GenericInterfaceDao {
 
