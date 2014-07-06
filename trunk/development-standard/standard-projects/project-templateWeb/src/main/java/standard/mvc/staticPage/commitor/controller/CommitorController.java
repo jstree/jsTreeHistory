124b1b76f170c254f53ef9e874762d474363a0de
gaia012 <gaia012@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-06 06:32:07 +0000

    commitor 페이지

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/commitor/controller/CommitorController.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/commitor/controller/CommitorController.java
@@ -1,44 +1,26 @@
-package standard.mvc.controller.spring.community.introduce;
+package standard.mvc.staticPage.commitor.controller;
 
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import standard.mvc.controller.GenericAbstractController;
 import standard.mvc.controller.GenericInterfaceController;
 import standard.mvc.dao.hibernate.SearchSupport;
 
-/**
- * 바로보드 소개 > 커미터 메뉴에 해당하는 컨트롤러
- * 
- * @author 이종렬
- * @since 2014.07.04
- */
 
-@Controller
-@RequestMapping(value = "/community/introduce/15commitor")
-public class CommitorController extends GenericAbstractController implements
-		GenericInterfaceController<Object> {
+public class CommitorController extends GenericAbstractController implements GenericInterfaceController<Object> {
 
 	@Override
-	@RequestMapping(value = "/select.do", method = RequestMethod.POST)
 	public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap,
 			HttpServletRequest request, HttpServletResponse response,
 			BindingResult bindingResult, Object parameterBean) {
-
-		String viewResolver = "/thymeleaf";
-		String site = "/community";
-		String menu = "/introduce";
-		String target = "/15commitor";
-
-		return viewResolver + site + menu + target;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -70,4 +52,5 @@ public class CommitorController extends GenericAbstractController implements
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 }
