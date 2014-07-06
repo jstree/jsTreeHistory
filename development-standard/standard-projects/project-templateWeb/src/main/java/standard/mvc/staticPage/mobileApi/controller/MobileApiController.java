519009bb685da4e73a794808e6c905b0ce63f845
holic <holic@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-06 08:30:11 +0000

    mobileApi 추가

---
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/mobileApi/controller/MobileApiController.java
@@ -0,0 +1,73 @@
+package standard.mvc.staticPage.mobileApi.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import standard.mvc.controller.GenericAbstractController;
+import standard.mvc.controller.GenericInterfaceController;
+import standard.mvc.dao.hibernate.SearchSupport;
+
+	/**
+	 * 바로보드 소개 > 모바일 API 디바이스 메뉴에 해당하는 컨트롤러
+	 * 
+	 * @author 유정우
+	 * @since 2014.07.06
+	 */
+
+	@Controller
+	@RequestMapping(value = "/community/introduce/11mobile-api")
+	public class MobileApiController extends GenericAbstractController implements
+			GenericInterfaceController<Object> {
+
+		@Override
+		@RequestMapping(value = "/select.do", method = RequestMethod.POST)
+		public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap,
+				HttpServletRequest request, HttpServletResponse response,
+				BindingResult bindingResult, Object parameterBean) {
+
+			String viewResolver = "/thymeleaf";
+			String site = "/community";
+			String menu = "/introduce";
+			String target = "/11mobile-api";
+
+			return viewResolver + site + menu + target;
+		}
+
+		@Override
+		public String invokeInsert(SearchSupport searchSupport, ModelMap modelMap,
+				HttpServletRequest request, HttpServletResponse response,
+				BindingResult bindingResult, Object parameterBean) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String invokeUpdate(SearchSupport searchSupport, ModelMap modelMap,
+				HttpServletRequest request, HttpServletResponse response,
+				BindingResult bindingResult, Object parameterBean) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String invokeDelete(SearchSupport searchSupport, ModelMap modelMap,
+				HttpServletRequest request, HttpServletResponse response,
+				BindingResult bindingResult, Object parameterBean) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Map<String, Map<String, Object>> bindTypes() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	}
