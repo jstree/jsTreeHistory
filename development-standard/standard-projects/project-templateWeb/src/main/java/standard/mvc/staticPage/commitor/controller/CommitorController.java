aa61a8eb360958fda372049877b8f909b88bb0af
JongRyul <gaia012@naver.com>
2014-07-06 06:48:33 +0000

    commitor 페이지

--- development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/commitor/controller/CommitorController.java
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/staticPage/commitor/controller/CommitorController.java
@@ -5,22 +5,41 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import standard.mvc.controller.GenericAbstractController;
 import standard.mvc.controller.GenericInterfaceController;
 import standard.mvc.dao.hibernate.SearchSupport;
 
+/**
+ * 바로보드 소개 > 커미터 메뉴에 해당하는 컨트롤러
+ * 
+ * @author 이종렬
+ * @since 2014.07.04
+ */
 
-public class CommitorController extends GenericAbstractController implements GenericInterfaceController<Object> {
+@Controller
+@RequestMapping(value = "/community/introduce/15commitor")
+public class CommitorController extends GenericAbstractController implements
+		GenericInterfaceController<Object> {
 
 	@Override
+	@RequestMapping(value = "/select.do", method = RequestMethod.POST)
 	public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap,
 			HttpServletRequest request, HttpServletResponse response,
 			BindingResult bindingResult, Object parameterBean) {
-		// TODO Auto-generated method stub
-		return null;
+
+		String viewResolver = "/thymeleaf";
+		String site = "/community";
+		String menu = "/introduce";
+		String target = "/15commitor";
+
+		return viewResolver + site + menu + target
+				;
 	}
 
 	@Override
@@ -52,5 +71,4 @@ public class CommitorController extends GenericAbstractController implements Gen
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 }
