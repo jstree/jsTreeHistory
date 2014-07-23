8d7c23e67521d0a4bc30f3ed31c04090ff3398b9
beryu82 <beryu82@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-23 13:02:18 +0000

    7.23 정원기 jstree 패키지 변경. 1. 테스트를 위해서는 일단 루트를 / 로 변경한다. 이유는 주소 호출에서 앞단에 domain을    해당 was에 맞게 변경하여 호출해주는 스크립트를 사용하지 않았기 때문이다. 2. 나머지 수정은 이전 변경사항에 적어둔 것을 따라하면 된다.

---
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/egovframework/com/ext/jstree/springiBatis/monitor/vo/T_JqGridRowData.java
@@ -0,0 +1,26 @@
+package egovframework.com.ext.jstree.springiBatis.monitor.vo;
+
+import java.util.List;
+
+public class T_JqGridRowData {
+
+	private String id;
+	private List<String> cell;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public List<String> getCell() {
+		return cell;
+	}
+
+	public void setCell(List<String> cell) {
+		this.cell = cell;
+	}
+
+}
