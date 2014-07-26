89bc0f6dc2a7119831576144bb1f267989b54a0d
today226 <6800272+today226@users.noreply.github.com>
2014-07-26 16:12:06 +0000

    [WWW313COKR][1.0.0-SNAPSHOT][오권우] css는 나스에 별도로 올렸습니다 IN PROGRESS - issue SP-12: RSS Widget 페이지를 구현한다 http://www.313.co.kr/jira/browse/

--- development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/business/widget/rss/controller/RssController.java
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/business/widget/rss/controller/RssController.java
@@ -7,15 +7,17 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sun.syndication.feed.synd.SyndCategoryImpl;
 import com.sun.syndication.feed.synd.SyndEntry;
@@ -24,14 +26,21 @@ import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 
+import standard.mvc.component.base.controller.GenericAbstractController;
+import standard.mvc.component.base.controller.GenericInterfaceController;
+import standard.mvc.component.base.dao.hibernate.SearchSupport;
+
 @Controller
-public class RssController {
-	
+@RequestMapping(value = { "**/community" })
+public class RssController  extends GenericAbstractController implements GenericInterfaceController<Object>{
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-	
-	@RequestMapping(value = "/RssController",  method=RequestMethod.POST)
-	public String RssReader(@RequestParam String url, Map<String, Object> model) throws IllegalArgumentException, FeedException, IOException {
-	
+	private static String url = null;
+
+	@RequestMapping(value = { "/component/business/widget/rss/bloter.do" }, method = { RequestMethod.GET, RequestMethod.POST }, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String BloterRssReader() throws IllegalArgumentException, FeedException, IOException {
+		
+		url = "http://feeds.feedburner.com/bloter";
 		URL feedUrl = new URL(url);
 		SyndFeedInput input = new SyndFeedInput();
 		SyndFeed syndFeed;
@@ -47,37 +56,97 @@ public class RssController {
         List<SyndEntry> entries = syndFeed.getEntries();        
         SyndEntry entry = null;
         
+        StringBuffer returnHtmlData = new StringBuffer();
+        returnHtmlData.append("<ul>");
         /*발행정보*/
-        for(int i=0, j=entries.size(); i<j ; i++) {
+        //for(int i=0, j=entries.size(); i<j ; i++) {
+        for(int i=0; i<5; i++){
             entry = entries.get(i);
             logger.debug("### getTitle   [" + entry.getTitle() +"]");
             logger.debug("### getUri    [" + entry.getUri() +"]");
             logger.debug("### getDescription  [" + stripTags(entry.getDescription().getValue()) +"]");
             //logger.debug("### getPublishedDate[" + entry.getPublishedDate().getTime() +"]");
             //logger.debug("### getContents[" + entry.getContents());
-            logger.debug("==================================================");
+            logger.debug("========================================{==========");
             
-            Map<String, String> xmlMap = new HashMap<String, String>();
-            xmlMap.put("Title", entry.getTitle());
-            xmlMap.put("Uri", entry.getUri());
-            xmlMap.put("Description", entry.getDescription().getValue());
-            
-            model.put("xmlMap", xmlMap);
-            
-            /*카테고리*/
-            StringBuffer cate = new StringBuffer();
-            if(entry.getCategories()!=null && entry.getCategories().size()>0){
-            	for(int ii=0,jj=entry.getCategories().size(); ii<jj; ii++){
-            		SyndCategoryImpl ss = (SyndCategoryImpl)(entry.getCategories().get(ii));
-            		cate.append(ss.getName().replace(" ", "")).append(",");
-            	}
-            }
-            logger.debug("### category  [" + cate.toString() +"]");
+            returnHtmlData.append("<li><a href=\"" + entry.getUri()+ "\"  target=\"_blank\" ");
+            returnHtmlData.append("onclick=\"window.open(this.href,'popupName', 'width=800,height=900'); ");
+            returnHtmlData.append("return false;\" onkeypress=\"this.oncilck()\"> " + entry.getTitle() + " </a></li>"); 
         }        
-        	
-		return "/test";
+        returnHtmlData.append("</ul>");
+		return returnHtmlData.toString();
 	}
 	
+	@RequestMapping(value = { "/component/business/widget/rss/zdnet.do" }, method = { RequestMethod.GET, RequestMethod.POST }, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String ZdnetRssReader() throws IllegalArgumentException, FeedException, IOException {
+		
+		url = "http://zdnetkorea.feedsportal.com/c/34249/f/622755/index.rss";
+		URL feedUrl = new URL(url);
+		SyndFeedInput input = new SyndFeedInput();
+		SyndFeed syndFeed;
+		syndFeed = input.build(new XmlReader(feedUrl));
+		
+        List<SyndEntry> entries = syndFeed.getEntries();        
+        SyndEntry entry = null;
+        
+        StringBuffer returnHtmlData = new StringBuffer();
+        returnHtmlData.append("<ul>");
+        /*발행정보*/
+        //for(int i=0, j=entries.size(); i<j ; i++) {
+        for(int i=0; i<5; i++){
+            entry = entries.get(i);
+            logger.debug("### getTitle   [" + entry.getTitle() +"]");
+            logger.debug("### getUri    [" + entry.getUri() +"]");
+            logger.debug("### getDescription  [" + stripTags(entry.getDescription().getValue()) +"]");
+            //logger.debug("### getPublishedDate[" + entry.getPublishedDate().getTime() +"]");
+            //logger.debug("### getContents[" + entry.getContents());
+            logger.debug("========================================{==========");
+            
+            returnHtmlData.append("<li><a href=\"" + entry.getUri()+ "\"  target=\"_blank\" ");
+            returnHtmlData.append("onclick=\"window.open(this.href,'popupName', 'width=800,height=900'); ");
+            returnHtmlData.append("return false;\" onkeypress=\"this.oncilck()\"> " + entry.getTitle() + " </a></li>");
+        }        
+        returnHtmlData.append("</ul>");
+		return returnHtmlData.toString();
+	}
+	
+	@RequestMapping(value = { "/component/business/widget/rss/google.do" }, method = { RequestMethod.GET, RequestMethod.POST }, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String FaceBookRssReader() throws IllegalArgumentException, FeedException, IOException {
+		
+		url = "http://www.inews24.com/rss/rss_computing.xml";
+		URL feedUrl = new URL(url);
+		SyndFeedInput input = new SyndFeedInput();
+		SyndFeed syndFeed;
+		syndFeed = input.build(new XmlReader(feedUrl));
+		
+        List<SyndEntry> entries = syndFeed.getEntries();        
+        SyndEntry entry = null;
+        
+        StringBuffer returnHtmlData = new StringBuffer();
+        returnHtmlData.append("<ul>");
+        /*발행정보*/
+        //for(int i=0, j=entries.size(); i<j ; i++) {
+        for(int i=0; i<5; i++){
+            entry = entries.get(i);
+            logger.debug("### getTitle   [" + entry.getTitle() +"]");
+            logger.debug("### getLink    [" + entry.getUri() +"]");
+            logger.debug("### getDescription  [" + stripTags(entry.getDescription().getValue()) +"]");
+            //logger.debug("### getPublishedDate[" + entry.getPublishedDate().getTime() +"]");
+            //logger.debug("### getContents[" + entry.getContents());
+            logger.debug("========================================{==========");
+            
+            returnHtmlData.append("<li><a href=\"" + entry.getUri()+ "\"  target=\"_blank\" ");
+            returnHtmlData.append("onclick=\"window.open(this.href,'popupName', 'width=800,height=900'); ");
+            returnHtmlData.append("return false;\" onkeypress=\"this.oncilck()\"> " + entry.getTitle() + " </a></li>");
+        }        
+        returnHtmlData.append("</ul>");
+		System.out.println(returnHtmlData.toString());
+        return returnHtmlData.toString();
+	}
+	
+
 	public static String stripTags(String text) {
 		if (text != null) {
 			return text.replaceAll("\\<.*?>","").trim();
@@ -85,4 +154,44 @@ public class RssController {
 			return "";
 		}
 	}
+	
+	
+	@Override
+	public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeInsert(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeUpdate(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeDelete(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Map<String, Object>> bindTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
