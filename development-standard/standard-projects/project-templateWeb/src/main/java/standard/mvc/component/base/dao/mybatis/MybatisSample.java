e1766a0267ae5086302b1863df1b91e8235e9d75
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-29 21:16:55 +0000

    IN PROGRESS - issue SP-27, SP-28 [WWW313COKR][1.0.0-SNAPSHOT][손호성] DAO, Service 고민중

--- development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/mybatis/MybatisSample.java
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/mybatis/MybatisSample.java
@@ -1,7 +1,45 @@
 package standard.mvc.component.base.dao.mybatis;
 
+import java.util.List;
+
 import standard.mvc.component.base.dao.GenericInterfaceDao;
 
 public class MybatisSample implements GenericInterfaceDao {
+
+	@Override
+	public Object invokeSelectOne(Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List invokeSelect(Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object invokeInsert(Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object invokeInsert(Object parameterBean, Object sequenceKey) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object invokeUpdate(Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object invokeDelete(Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
