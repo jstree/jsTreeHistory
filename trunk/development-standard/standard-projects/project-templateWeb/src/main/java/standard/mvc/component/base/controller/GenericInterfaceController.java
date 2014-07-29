e763681ae37e15248c5e446cc5aedcd3c296f2c5
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-07-29 01:09:22 +0000

    SP-26 #resolve #comment [1.0.0-SNAPSHOT][이동민] 표준 컨트롤러를 구현 및 정의. Abstract, Interface 구현 및 적용. Line Width를 정리.

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/controller/GenericInterfaceController.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/controller/GenericInterfaceController.java
@@ -5,15 +5,32 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import standard.mvc.component.base.dao.hibernate.SearchSupport;
 
 public interface GenericInterfaceController<T> {
 
-	
-	public String invokeSelect(@RequestAttribute("searchSupport") SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, T parameterBean);
-	public String invokeInsert(@RequestAttribute("searchSupport") SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, T parameterBean);
-	public String invokeUpdate(@RequestAttribute("searchSupport") SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, T parameterBean);
-	public String invokeDelete(@RequestAttribute("searchSupport") SearchSupport searchSupport, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response, BindingResult bindingResult, T parameterBean);
+	public String invokeSelect(
+			@RequestAttribute("searchSupport") SearchSupport searchSupport,
+			ModelMap modelMap, HttpServletRequest request,
+			HttpServletResponse response, BindingResult bindingResult,
+			T parameterBean);
+
+	public String invokeInsert(
+			@RequestAttribute("searchSupport") SearchSupport searchSupport,
+			ModelMap modelMap, HttpServletRequest request,
+			HttpServletResponse response, BindingResult bindingResult,
+			T parameterBean);
+
+	public String invokeUpdate(
+			@RequestAttribute("searchSupport") SearchSupport searchSupport,
+			ModelMap modelMap, HttpServletRequest request,
+			HttpServletResponse response, BindingResult bindingResult,
+			T parameterBean);
+
+	public String invokeDelete(
+			@RequestAttribute("searchSupport") SearchSupport searchSupport,
+			ModelMap modelMap, HttpServletRequest request,
+			HttpServletResponse response, BindingResult bindingResult,
+			T parameterBean);
 }
