2a60f40b4a30318a4190f7f80da468f210e25892
holic <holic@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-08-02 13:59:31 +0000

    SP-21 #resolve #comment [WWW313COKR][1.0.0-SNAPSHOT][유정우] JAVADOC 수정.

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/egovframework/com/ext/jstree/strutsiBatis/dao/I_DB_SearchNode.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/egovframework/com/ext/jstree/strutsiBatis/dao/I_DB_SearchNode.java
@@ -8,17 +8,19 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
+/**
+ * node검색 interface
+ * */
 public interface I_DB_SearchNode extends
-        I_GenericDao<T_ComprehensiveTree, P_ComprehensiveTree>
-{
-    
-    static Logger logger = Logger.getLogger(I_DB_GetChildNode.class);
-    
-    public List<T_ComprehensiveTree> searchNodeByString(
-            P_ComprehensiveTree p_ComprehensiveTree, String determineDBSetting);
-    
-    public List<String> searchNodeByPosition(
-            List<P_ComprehensiveTree> p_SearchNodeByPositions,
-            String determineDBSetting);
-    
+		I_GenericDao<T_ComprehensiveTree, P_ComprehensiveTree> {
+
+	static Logger logger = Logger.getLogger(I_DB_GetChildNode.class);
+
+	public List<T_ComprehensiveTree> searchNodeByString(
+			P_ComprehensiveTree p_ComprehensiveTree, String determineDBSetting);
+
+	public List<String> searchNodeByPosition(
+			List<P_ComprehensiveTree> p_SearchNodeByPositions,
+			String determineDBSetting);
+
 }
