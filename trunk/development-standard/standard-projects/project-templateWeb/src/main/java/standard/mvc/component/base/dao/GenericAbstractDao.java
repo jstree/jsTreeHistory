6f7f196b16778b972a572bfcf8260226f4af4185
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-08-03 13:23:35 +0000

    SP-28 #resolve #comment [WWW313COKR][1.0.0-SNAPSHOT][손호성] AbstractDao 에 mapper 와 rowbounds 를 구겨넣을 방법을 고민하다가, 추상화에 반한다는 판단이 서서 배제

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/GenericAbstractDao.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/dao/GenericAbstractDao.java
@@ -1,16 +1,12 @@
 package standard.mvc.component.base.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 
-public abstract class GenericAbstractDao<T, C, P> {
+public abstract class GenericAbstractDao<T, C> {
 
-	protected T legacyConnection;
-	
 	@SuppressWarnings("unchecked")
-	public void invokeSelect(String statementName, Object parameterBean, C ... command) throws DataAccessException {
-	}
-	
-	public void initLegacyConnection(P properties) throws DataAccessException {
+	public void invokeSelect(String statementName, T parameterBean, C ... command) throws DataAccessException {
 	}
 
 }
