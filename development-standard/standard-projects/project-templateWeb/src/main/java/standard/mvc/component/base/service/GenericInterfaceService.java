248489b1a9b4315903b365c876af3e2ca6b23db7
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-08-03 13:31:24 +0000

    SP-27 #resolve #comment [WWW313COKR][1.0.0-SNAPSHOT][손호성] AbstractService 에서 공통작업의 범위가 어디까지인지 고민하다가 암/복호화 (예시 이외의 작업은 override 필요), 날짜포맷구성, 외부 리소스 import (rest uri or hadoop output) 로 한정.

--- development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/service/GenericInterfaceService.java
+++ development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/base/service/GenericInterfaceService.java
@@ -4,7 +4,20 @@ import java.util.List;
 
 import standard.mvc.component.base.exception.GenericServiceRuntimeException;
 
-public interface GenericInterfaceService<T> {
-	List<T> getResults(T modelVO) throws GenericServiceRuntimeException;
-	void validate(T modelVO) throws GenericServiceRuntimeException;
+public interface GenericInterfaceService<V, T, C> {
+	
+	List<V> getResults(V modelVO) throws GenericServiceRuntimeException;
+	
+	void validate(V modelVO) throws GenericServiceRuntimeException;
+
+	T invokeSelectOne(T parameterBean);
+	
+	List<T> invokeSelect(T parameterBean);
+	
+	T invokeInsert(T parameterBean);
+	
+	C invokeUpdate(T parameterBean);
+	
+	C invokeDelete(T parameterBean);
+
 }
