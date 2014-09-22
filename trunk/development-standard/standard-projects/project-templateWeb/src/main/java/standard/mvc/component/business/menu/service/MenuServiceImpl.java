14e5e87d2c0bcbaf9ae27196c7bafbc69763c6b0
cyanism <cyanism@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-09-22 12:40:49 +0000

    [1.0.0-SNAPSHOT][류강하] 최신 소스 코드 커밋

--- trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/business/menu/service/MenuServiceImpl.java
+++ trunk/development-standard/standard-projects/project-templateWeb/src/main/java/standard/mvc/component/business/menu/service/MenuServiceImpl.java
@@ -1,7 +1,9 @@
 package standard.mvc.component.business.menu.service;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
 
@@ -29,6 +31,8 @@ import standard.mvc.component.business.menu.vo.MenuComprehensiveTree;
  *  -------      ------------   -----------------------
  *  2014. 9. 05.  이동민               최초 생성
  *  2014. 9. 15.  류강하               노드 추가 메서드 추가 및 주석
+ *  2014. 9. 20.  류강하               노드 추가 관련 메서드 추가
+ *  2014. 9. 21.  김대근               removeNode 메소드 추가
  * 
  *  Copyright (C) 2014 by 313 DeveloperGroup  All right reserved.
  * </pre>
@@ -47,104 +51,225 @@ public class MenuServiceImpl implements MenuService{
 	}
 
     @Override
-    public MenuComprehensiveTree addNode(MenuComprehensiveTree menuComprehensiveTree) {
+   public MenuComprehensiveTree addNode(MenuComprehensiveTree menuComprehensiveTree) {
         
-//        MenuComprehensiveTree nodeById  = menuDao.getNode(menuComprehensiveTree);
-//        MenuComprehensiveTree nodeByRef = menuDao.getNodeByRef(menuComprehensiveTree);
-//
-//        List<MenuComprehensiveTree> childNodesFromNodeByRef = menuDao.getChildNode(nodeByRef);
-//        
-//        MenuComprehensiveTree t_ComprehensiveTree = new MenuComprehensiveTree();
-//        
-//        int spaceOfTargetNode = 2;
-//        Collection<Integer> c_idsByChildNodeFromNodeById = null;
-//
-//        if( nodeById != null && comprehensiveTree.getCopy() == 0 ) {
-//            
-//            this.cutMyself( nodeById, spaceOfTargetNode, c_idsByChildNodeFromNodeById );
-//        }
-//
-//        this.stretchPositionForMyselfFromJstree( c_idsByChildNodeFromNodeById, nodeById, comprehensiveTree );
-//
-//        int rightPositionFromNodeByRef = nodeByRef.getC_right();
-//        rightPositionFromNodeByRef = Math.max(rightPositionFromNodeByRef, 1);
-//
-//        int self = ( nodeById != null
-//                  && !comprehensiveTree.getCopyBooleanValue()
-//                  && nodeById.getC_parentid() == comprehensiveTree.getRef() 
-//                  && comprehensiveTree.getC_position() > nodeById.getC_position() ) ? 1 : 0;
-//
-//        for (MenuComprehensiveTree child : childNodesFromNodeByRef) {
-//            
-//            if (child.getC_position() - self == comprehensiveTree.getC_position()) {
-//                rightPositionFromNodeByRef = child.getC_left();
-//                break;
-//            }
-//        }
-//
-//        if (nodeById != null && !comprehensiveTree.getCopyBooleanValue()
-//                && nodeById.getC_left() < rightPositionFromNodeByRef) {
-//            rightPositionFromNodeByRef -= spaceOfTargetNode;
-//        }
-//
-//        this.stretchLeftRightForMyselfFromJstree( spaceOfTargetNode
-//                                                , rightPositionFromNodeByRef
-//                                                , comprehensiveTree.getCopy()
-//                                                , c_idsByChildNodeFromNodeById );
-//
-//        int targetNodeLevel = comprehensiveTree.getRef() == 0 ? 0   : nodeByRef.getC_level() + 1;
-//        int comparePosition = rightPositionFromNodeByRef;
-//        
-//        if (nodeById != null) {
-//
-//            targetNodeLevel = nodeById.getC_level() - (nodeByRef.getC_level() + 1);
-//            comparePosition = nodeById.getC_left()  - rightPositionFromNodeByRef;
-//            
-//            if (comprehensiveTree.getCopyBooleanValue()) {
-//                int ind = this.pasteMyselfFromJstree( comprehensiveTree.getRef()
-//                                                    , comparePosition
-//                                                    , spaceOfTargetNode
-//                                                    , targetNodeLevel
-//                                                    , rightPositionFromNodeByRef
-//                                                    , c_idsByChildNodeFromNodeById
-//                                                    , nodeById );
-//                t_ComprehensiveTree.setId(ind);
-//                this.fixCopy( ind, comprehensiveTree.getC_position() );
-//            } else {
-//                this.enterMyselfFromJstree( comprehensiveTree.getRef()
-//                                          , comprehensiveTree.getC_position()
-//                                          , comprehensiveTree.getC_id()
-//                                          , comparePosition
-//                                          , targetNodeLevel
-//                                          , c_idsByChildNodeFromNodeById );
-//            }
-//        } else {
-//            comprehensiveTree.setC_parentid(comprehensiveTree.getRef());
-//            comprehensiveTree.setC_left(comparePosition);
-//            comprehensiveTree.setC_right(comparePosition + 1);
-//            comprehensiveTree.setC_level(targetNodeLevel);
-//
-//            int insertSeqResult = menuDao.addMyselfFromJstree(comprehensiveTree);
-//
-//            t_ComprehensiveTree.setId(insertSeqResult);
-//            comprehensiveTree.setC_id(insertSeqResult);
-//            int alterCountResult = menuDao.alterNode(comprehensiveTree);
-//
-//            if (insertSeqResult > 0 && alterCountResult == 1) {
-//                t_ComprehensiveTree.setStatus(1);
-//            } else {
-//                throw new RuntimeException("심각한 오류 발생 - 삽입 노드");
-//            }
-//        }
-//
-//        if (comprehensiveTree.getCopyBooleanValue()) {
-//            this.fixCopy( comprehensiveTree.getC_id(), comprehensiveTree.getC_position() );
-//        }
-//        return t_ComprehensiveTree;
+        MenuComprehensiveTree nodeById = menuDao.getNode(menuComprehensiveTree);
+        MenuComprehensiveTree nodeByRef = menuDao.getNodeByRef(menuComprehensiveTree);
+
+        List<MenuComprehensiveTree> childNodesFromNodeByRef = menuDao.getChildNode(nodeByRef);
         
-        return null;
+        MenuComprehensiveTree t_ComprehensiveTree = new MenuComprehensiveTree();
+        
+        int spaceOfTargetNode = 2;
+        Collection<Integer> c_idsByChildNodeFromNodeById = null;
+
+        if (nodeById != null && menuComprehensiveTree.getCopy() == 0) {
+            this.cutMyself(nodeById, spaceOfTargetNode, c_idsByChildNodeFromNodeById);
+        }
+
+        this.stretchPositionForMyselfFromJstree(c_idsByChildNodeFromNodeById, nodeById, menuComprehensiveTree);
+
+        int rightPositionFromNodeByRef = nodeByRef.getC_right();
+        rightPositionFromNodeByRef = Math.max(rightPositionFromNodeByRef, 1);
+
+        int self = ( nodeById != null
+                  && !menuComprehensiveTree.getCopyBooleanValue()
+                  && nodeById.getC_parentid() == menuComprehensiveTree.getRef() 
+                  && menuComprehensiveTree.getC_position() > nodeById.getC_position() ) ? 1 : 0;
+
+        for (MenuComprehensiveTree child : childNodesFromNodeByRef) {
+            
+            if (child.getC_position() - self == menuComprehensiveTree.getC_position()) {
+                rightPositionFromNodeByRef = child.getC_left();
+                break;
+            }
+        }
+
+        if (nodeById != null && !menuComprehensiveTree.getCopyBooleanValue()
+                && nodeById.getC_left() < rightPositionFromNodeByRef) {
+            rightPositionFromNodeByRef -= spaceOfTargetNode;
+        }
+
+        this.stretchLeftRightForMyselfFromJstree( spaceOfTargetNode
+                                                , rightPositionFromNodeByRef
+                                                , menuComprehensiveTree.getCopy()
+                                                , c_idsByChildNodeFromNodeById );
+
+        int targetNodeLevel = menuComprehensiveTree.getRef() == 0 ? 0   : nodeByRef.getC_level() + 1;
+        int comparePosition = rightPositionFromNodeByRef;
+        
+        if (nodeById != null) {
+
+            targetNodeLevel = nodeById.getC_level() - (nodeByRef.getC_level() + 1);
+            comparePosition = nodeById.getC_left()  - rightPositionFromNodeByRef;
+            
+            if (menuComprehensiveTree.getCopyBooleanValue()) {
+                int ind = this.pasteMyselfFromJstree( menuComprehensiveTree.getRef()
+                                                    , comparePosition
+                                                    , spaceOfTargetNode
+                                                    , targetNodeLevel
+                                                    , rightPositionFromNodeByRef
+                                                    , c_idsByChildNodeFromNodeById
+                                                    , nodeById );
+                t_ComprehensiveTree.setId(ind);
+                this.fixCopy( ind, menuComprehensiveTree.getC_position() );
+            } else {
+                this.enterMyselfFromJstree( menuComprehensiveTree.getRef()
+                                          , menuComprehensiveTree.getC_position()
+                                          , menuComprehensiveTree.getC_id()
+                                          , comparePosition
+                                          , targetNodeLevel
+                                          , c_idsByChildNodeFromNodeById );
+            }
+        } else {
+            menuComprehensiveTree.setC_parentid(menuComprehensiveTree.getRef());
+            menuComprehensiveTree.setC_left(comparePosition);
+            menuComprehensiveTree.setC_right(comparePosition + 1);
+            menuComprehensiveTree.setC_level(targetNodeLevel);
+
+            int insertSeqResult = menuDao.addMyselfFromJstree(menuComprehensiveTree);
+
+            t_ComprehensiveTree.setId(insertSeqResult);
+            menuComprehensiveTree.setC_id(insertSeqResult);
+            int alterCountResult = menuDao.alterNode(menuComprehensiveTree);
+
+            if (insertSeqResult > 0 && alterCountResult == 1) {
+                t_ComprehensiveTree.setStatus(1);
+            } else {
+                throw new RuntimeException("심각한 오류 발생 - 삽입 노드");
+            }
+        }
+
+        if (menuComprehensiveTree.getCopyBooleanValue()) {
+            this.fixCopy( menuComprehensiveTree.getC_id(), menuComprehensiveTree.getC_position() );
+        }
+        return t_ComprehensiveTree;
     }
 	
+    
+    private void cutMyself(MenuComprehensiveTree nodeById, int spaceOfTargetNode,
+            Collection<Integer> c_idsByChildNodeFromNodeById) {
+
+        nodeById.setSpaceOfTargetNode(spaceOfTargetNode);
+        nodeById.setC_idsByChildNodeFromNodeById(c_idsByChildNodeFromNodeById);
+
+        menuDao.cutMyself(nodeById);
+    }
+    
+    private void stretchPositionForMyselfFromJstree(Collection<Integer> c_idsByChildNodeFromNodeById,
+            MenuComprehensiveTree nodeById, MenuComprehensiveTree comprehensiveTree) {
+
+        comprehensiveTree.setC_idsByChildNodeFromNodeById(c_idsByChildNodeFromNodeById);
+        comprehensiveTree.setNodeById(nodeById);
+
+        menuDao.stretchPositionForMyselfFromJstree(comprehensiveTree);
+    }
+    
+    private void stretchLeftRightForMyselfFromJstree(int spaceOfTargetNode, int rightPositionFromNodeByRef, int copy,
+            Collection<Integer> c_idsByChildNodeFromNodeById) {
+        
+        MenuComprehensiveTree onlyStretchLeftRightForMyselfFromJstree = new MenuComprehensiveTree();
+        
+        onlyStretchLeftRightForMyselfFromJstree.setSpaceOfTargetNode(            spaceOfTargetNode            );
+        onlyStretchLeftRightForMyselfFromJstree.setRightPositionFromNodeByRef(   rightPositionFromNodeByRef   );
+        onlyStretchLeftRightForMyselfFromJstree.setC_idsByChildNodeFromNodeById( c_idsByChildNodeFromNodeById );
+        onlyStretchLeftRightForMyselfFromJstree.setCopy(copy);
+        
+        menuDao.stretchLeftRightForMyselfFromJstree(onlyStretchLeftRightForMyselfFromJstree);
+    }
+    
+    private int pasteMyselfFromJstree(int ref, int idif, int spaceOfTargetNode, int ldif, int rightPositionFromNodeByRef,
+            Collection<Integer> c_idsByChildNodeFromNodeById, MenuComprehensiveTree nodeById) {
+
+        MenuComprehensiveTree onlyPasteMyselfFromJstree = new MenuComprehensiveTree();
+        
+        onlyPasteMyselfFromJstree.setRef(ref);
+        onlyPasteMyselfFromJstree.setIdif(idif);
+        onlyPasteMyselfFromJstree.setSpaceOfTargetNode(spaceOfTargetNode);
+        onlyPasteMyselfFromJstree.setLdif(ldif);
+        onlyPasteMyselfFromJstree.setC_idsByChildNodeFromNodeById( c_idsByChildNodeFromNodeById );
+        onlyPasteMyselfFromJstree.setRightPositionFromNodeByRef(   rightPositionFromNodeByRef   );
+        onlyPasteMyselfFromJstree.setNodeById(nodeById);
+        
+        onlyPasteMyselfFromJstree.setIdifLeft( idif + (nodeById.getC_left() >= rightPositionFromNodeByRef ? spaceOfTargetNode   : 0) );
+        onlyPasteMyselfFromJstree.setIdifRight(idif + (nodeById.getC_left() >= rightPositionFromNodeByRef ? spaceOfTargetNode   : 0) );
+        
+        return menuDao.pasteMyselfFromJstree(onlyPasteMyselfFromJstree);
+    }   
+
+    private void enterMyselfFromJstree(int ref, int c_position, int c_id, int idif, int ldif, 
+            Collection<Integer> c_idsByChildNodeFromNodeById) {
+
+        MenuComprehensiveTree onlyPasteMyselfFromJstree = new MenuComprehensiveTree();
+        onlyPasteMyselfFromJstree.setRef(ref);
+        onlyPasteMyselfFromJstree.setC_position(c_position);
+        onlyPasteMyselfFromJstree.setC_id(c_id);
+        onlyPasteMyselfFromJstree.setIdif(idif);
+        onlyPasteMyselfFromJstree.setLdif(ldif);
+        onlyPasteMyselfFromJstree.setC_idsByChildNodeFromNodeById( c_idsByChildNodeFromNodeById );
+
+        menuDao.enterMyselfFromJstree(onlyPasteMyselfFromJstree);
+    }  
+    
+    private void fixCopy(int ind, int ref) {
+        
+        MenuComprehensiveTree comprehensiveTree = new MenuComprehensiveTree();
+        comprehensiveTree.setC_id(ind);
+
+        MenuComprehensiveTree node = menuDao.getNode(comprehensiveTree);
+
+        List<MenuComprehensiveTree> children = menuDao.getChildNodeByLeftRight(node);
+
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = node.getC_left() + 1; i < node.getC_right(); i++) {
+            map.put(i, ind);
+        }
+
+        for (int i = 0; i < children.size(); i++) {
+
+            MenuComprehensiveTree child = children.get(i);
+
+            if (child.getC_id() == ind) {
+                logger.debug(">>>>>>>>>>>>>>>>> 기준노드가 잡혔음.");
+                logger.debug("C_TITLE    = " + child.getC_title());
+                logger.debug("C_ID       = " + ind);
+                logger.debug("C_POSITION = " + ref);
+                
+                MenuComprehensiveTree onlyFixCopyFromJstree = new MenuComprehensiveTree();
+                onlyFixCopyFromJstree.setFixCopyId(ind);
+                onlyFixCopyFromJstree.setFixCopyPosition(ref);
+                
+                menuDao.fixCopyIF(onlyFixCopyFromJstree);
+                continue;
+            }
+            logger.debug(">>>>>>>>>>>>>>>>> 기준노드 아래 있는 녀석임");
+            logger.debug("C_TITLE    = " + child.getC_title());
+            logger.debug("C_ID       = " + ind);
+            logger.debug("C_POSITION = " + ref);
+            logger.debug("부모아이디값 = " + map.get(child.getC_left()));
+            
+            child.setFixCopyId(map.get(child.getC_left()));
+            
+            menuDao.fixCopy(child);
+            
+            for (int j = child.getC_left() + 1; j < child.getC_right(); j++) {
+                map.put(j, child.getC_id());
+            }
+        }
+    }
+
+	@Override
+	public int removeNode(MenuComprehensiveTree menuComprehensiveTree) {
+		return menuDao.removeNode(menuComprehensiveTree);
+	}
+    
+  
+    
+    
+    
+    
+    
+    
 //	@Override
 //	public List<String> searchNode(MenuComprehensiveTree menuComprehensiveTree) {
 //		
