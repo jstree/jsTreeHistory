090ddc15bbadaa6d427c7fb7d1d090ec2b10d9f7
goldpig22 <goldpig22@ae997c0a-2d90-be44-948b-7632c96526aa>
2014-11-17 14:31:17 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][김대근] [BT] SP-53 #comment 커미터 스큐쥴러 관련 컨트럴러 패키지 경로 변경 [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-war/src/main/java/standard/mvc/component/business/community/schedule/controller/ScheduleManageController.java
+++ trunk/development-standard/standard-projects/project-war/src/main/java/standard/mvc/component/business/community/schedule/controller/ScheduleManageController.java
@@ -1,25 +1,25 @@
-package standard.mvc.component.business.schdulManage.controller;
+package standard.mvc.component.business.community.schedule.controller;
 
 import java.util.Calendar;
-import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import standard.mvc.component.base.controller.GenericAbstractController;
+import standard.mvc.component.base.controller.GenericInterfaceController;
+import standard.mvc.component.base.dao.hibernate.SearchSupport;
 
 
 import egovframework.com.cmm.ComDefaultCodeVO;
-import egovframework.com.cmm.ComDefaultVO;
 import egovframework.com.cmm.EgovMessageSource;
 import egovframework.com.cmm.service.EgovCmmUseService;
 import egovframework.com.cmm.util.EgovUserDetailsHelper;
@@ -48,7 +48,7 @@ import egovframework.rte.fdl.cmmn.exception.EgovBizException;
  * </pre>
  */
 @Controller
-public class SchdulManageController extends GenericAbstractController{
+public class ScheduleManageController extends GenericAbstractController implements GenericInterfaceController<Object>{
 	
 	/** EgovMessageSource */
     @Resource(name="egovMessageSource")
@@ -60,6 +60,40 @@ public class SchdulManageController extends GenericAbstractController{
 	@Resource(name = "egovIndvdlSchdulManageService")
 	private EgovIndvdlSchdulManageService egovIndvdlSchdulManageService;
 
+
+
+	@Override
+	public String invokeSelect(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeInsert(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeUpdate(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String invokeDelete(SearchSupport searchSupport, ModelMap modelMap,
+			HttpServletRequest request, HttpServletResponse response,
+			BindingResult bindingResult, Object parameterBean) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	@Override
 	public Map<String, Map<String, Object>> bindTypes() {
 		// TODO Auto-generated method stub
@@ -69,7 +103,7 @@ public class SchdulManageController extends GenericAbstractController{
 	@RequestMapping("/committerSchedule.do")
 	public String getCommitterSchedule(ModelMap model) throws Exception {
         
-		return "/jsp/cop/smt/sim/committerSchedule";
+		return "/jsp/community/aboutUs/schedule/committerSchedule";
 	}
 	
 	/**
@@ -85,8 +119,6 @@ public class SchdulManageController extends GenericAbstractController{
 	public String getBaroIndvdlSchdulManageMonthList(ModelMap model,@RequestParam Map<String, Object> commandMap)
 			 throws Exception {
 
-		if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
-    	
 		//일정구분 검색 유지
         model.addAttribute("searchKeyword", StringUtils.isEmpty(commandMap.get("searchKeyword")) ? "" : (String)commandMap.get("searchKeyword"));
         model.addAttribute("searchCondition", StringUtils.isEmpty(commandMap.get("searchCondition")) ? "" : (String)commandMap.get("searchCondition"));
@@ -141,7 +173,7 @@ public class SchdulManageController extends GenericAbstractController{
         cal.set(Calendar.DATE, endDay);
         model.addAttribute("lastWeek", cal.get(Calendar.WEEK_OF_MONTH));
         
-		return "/jsp/cop/smt/sim/committerScheduleMonthList";
+		return "/jsp/community/aboutUs/schedule/committerScheduleMonthList";
 	}
 	
 	/**
@@ -178,22 +210,6 @@ public class SchdulManageController extends GenericAbstractController{
     	
     	model.addAllAttributes(commandMap);
     	
-		return "/jsp/cop/smt/sim/committerSchdulManageDetail"; 	
+		return "/jsp/community/aboutUs/schedule/committerScheduleManageDetail"; 	
 	}
-	
-	/**
-     * 운영자 권한을 확인한다.(로그인 여부를 확인한다.)
-     * 
-     * @param boardMaster
-     * @throws EgovBizException
-     */
-    protected boolean checkAuthority(ModelMap model) throws Exception {
-    	// 사용자권한 처리
-    	if(!EgovUserDetailsHelper.isAuthenticated()) {
-    		model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
-        	return false; 
-    	}else{
-    		return true;
-    	}
-    }
 }
