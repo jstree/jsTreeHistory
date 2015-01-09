ffeb50be2e9f03b4e2e3b4c812a2e23ef6ed6601
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-01-09 07:27:13 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-56 #resolve #time 3h #comment 설정 클래스 분리. [RV] +review SR @spear79

---
+++ trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/config/WebApplicationContextConfig.java
@@ -0,0 +1,24 @@
+package egovframework.com.ext.jstree.support.manager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource({
+		"classpath:/META-INF/egovframework/spring/${profile.active.mode}-spring-viewResolver.properties",
+		"classpath:/META-INF/egovframework/egovProps/globals.properties" })
+@ImportResource({ "classpath*:/META-INF/egovframework/spring/com/context-*.xml" })
+public class WebApplicationContextConfig {
+
+	public WebApplicationContextConfig() {
+		super();
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer webMobilePropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+}
