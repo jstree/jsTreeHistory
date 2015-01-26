a045d6cd9e42d420bcd8644dace3ce452cef8752
cyanism <cyanism@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-01-26 22:51:57 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][류강하] [BT] SP-87 #comment JsTree Spring+iBatis Core Service DB 연동하지 않는 테스트 작성 중 [RV] +review SR @spear79

---
+++ trunk/development-standard/standard-projects/project-war/src/test/java/egovframework/com/ext/jstree/springiBatis/core/CoreServiceTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package egovframework.com.ext.jstree.springiBatis.core;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import egovframework.com.ext.jstree.springiBatis.core.service.CoreServiceImpl;
+import egovframework.com.ext.jstree.springiBatis.core.vo.ComprehensiveTree;
+
+/**
+ * Modification Information
+ * 
+ * @author 류강하
+ * @since 2015. 1. 27.
+ * @version 1.0
+ * @see <pre>
+ *  Class Name  : CoreServiceTest.java
+ *  Description : JsTree Spring+iBatis 버젼의 JUnit4 테스트 클래스
+ *  Infomation  : 애플리케이션 컨텍스트를 사용하지 않고, DB를 사용하지 않는 Mock DAO를 사용하여 JsTree 코어 서비스를 검증하는 테스트
+ * 
+ *  << 개정이력(Modification Information) >>
+ *  
+ *  수정일               수정자                 수정내용
+ *  -------       ------------   -----------------------
+ *  2015. 1. 27.  류강하                 최초 생성
+ * 
+ *  Copyright (C) 2015 by 313 DeveloperGroup  All right reserved.
+ * </pre>
+ */
+public class CoreServiceTest {
+
+    private static CoreServiceImpl coreService = new CoreServiceImpl();
+    private static MockCoreDao<ComprehensiveTree> mockCoreDao = new MockCoreDao<>();
+    
+    private static ComprehensiveTree rootNodeStored;
+    private static ComprehensiveTree firstChildNodeStored;
+    
+    @Before
+    public void setUp() throws Exception {
+
+        Field coreDaoField = coreService.getClass().getDeclaredField("coreDao");
+        
+        coreDaoField.setAccessible(true);
+        coreDaoField.set(coreService, mockCoreDao);
+        
+        ComprehensiveTree pRootNode = new ComprehensiveTree();
+        pRootNode.setC_id(1);
+        
+        // TODO Core Service에 getNode()가 없기 때문에 Core DAO를 직접 사용해야 함.
+        rootNodeStored = mockCoreDao.getNode(pRootNode);
+        
+        assertSame(rootNodeStored.getC_id(), 1);
+        assertSame(rootNodeStored.getC_type(), null);
+        assertSame(rootNodeStored.getC_left(), 1);
+        assertSame(rootNodeStored.getC_right(), 4);
+        assertSame(rootNodeStored.getC_position(), 0);
+        assertEquals(rootNodeStored.getC_title(), "Root Node");
+        assertSame(rootNodeStored.getC_parentid(), 0);
+        
+        // TODO 또는 getChildNode()의 index 0을 사용해서 getNode()의 기능을 대신해야 함.
+        List<ComprehensiveTree> childNodes = coreService.getChildNode(pRootNode);
+        
+        assertSame(childNodes.size(), 1);
+        
+        firstChildNodeStored = childNodes.get(0);
+        
+        assertSame(firstChildNodeStored.getC_id(), 2);
+        assertEquals(firstChildNodeStored.getC_type(), "drive");
+        assertSame(firstChildNodeStored.getC_left(), 2);
+        assertSame(firstChildNodeStored.getC_right(), 3);
+        assertSame(firstChildNodeStored.getC_position(), 0);
+        assertEquals(firstChildNodeStored.getC_title(), "First Child Node");
+        assertSame(firstChildNodeStored.getC_parentid(), 1);
+    }
+    
+    @Test
+    public void simpleAddNode() throws Exception {
+        
+        // TODO position을 생성해줄 유틸이 필요하여 만듦.
+        int position = mockCoreDao.generatePosition(firstChildNodeStored);
+        
+        ComprehensiveTree defaultNode = new ComprehensiveTree();
+        defaultNode.setC_type("default");
+        defaultNode.setC_position(position);
+        defaultNode.setC_title("defaultNode");
+        defaultNode.setRef(firstChildNodeStored.getC_id());
+        
+        coreService.addNode(defaultNode);
+        
+        ComprehensiveTree defaultNodeStored = coreService.getChildNode(firstChildNodeStored).get(0);
+        
+        assertSame(defaultNodeStored.getC_id(), 3);
+        assertEquals(defaultNodeStored.getC_type(), "default");
+        assertSame(defaultNodeStored.getC_left(), 3);
+        assertSame(defaultNodeStored.getC_right(), 4);
+        assertSame(defaultNodeStored.getC_position(), 1);
+        assertEquals(defaultNodeStored.getC_title(), "defaultNode");
+        assertSame(defaultNodeStored.getC_parentid(), firstChildNodeStored.getC_id());
+        
+        assertSame(rootNodeStored.getC_left(), 1);
+        assertSame(rootNodeStored.getC_right(), 6);
+        
+        assertSame(firstChildNodeStored.getC_left(), 2);
+        assertSame(firstChildNodeStored.getC_right(), 5);
+    }
+}
---
+++ trunk/development-standard/standard-projects/project-war/src/test/java/egovframework/com/ext/jstree/springiBatis/core/MockCoreDao.java
@@ -0,0 +1,316 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package egovframework.com.ext.jstree.springiBatis.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import egovframework.com.ext.jstree.springiBatis.core.dao.CoreDao;
+import egovframework.com.ext.jstree.springiBatis.core.vo.ComprehensiveTree;
+
+/**
+ * Modification Information
+ * 
+ * @author 류강하
+ * @since 2015. 1. 27.
+ * @version 1.0
+ * @param <T>
+ * @see <pre>
+ *  Class Name  : MockCoreDao.java
+ *  Description : JsTree Spring+iBatis 버젼의 JUnit4 테스트 클래스
+ *  Infomation  : JsTree 코어 서비스를 검증하는데 사용되는, DB를 사용하지 않는 Mock DAO
+ * 
+ *  << 개정이력(Modification Information) >>
+ *  
+ *  수정일               수정자                 수정내용
+ *  -------       ------------   -----------------------
+ *  2015. 1. 27.  류강하                 최초 생성
+ * 
+ *  Copyright (C) 2015 by 313 DeveloperGroup  All right reserved.
+ * </pre>
+ */
+public class MockCoreDao<T> implements CoreDao {
+
+    private List<ComprehensiveTree> tree = new ArrayList<>();
+    
+    public MockCoreDao() {
+        ComprehensiveTree rootNode = new ComprehensiveTree();
+        rootNode.setC_id(1);
+        rootNode.setC_type(null);
+        rootNode.setC_left(1);
+        rootNode.setC_right(4);
+        rootNode.setC_position(0);
+        rootNode.setC_title("Root Node");
+        rootNode.setC_parentid(0);
+        tree.add(rootNode);
+        
+        ComprehensiveTree firstChildNode = new ComprehensiveTree();
+        firstChildNode.setC_id(2);
+        firstChildNode.setC_type("drive");
+        firstChildNode.setC_left(2);
+        firstChildNode.setC_right(3);
+        firstChildNode.setC_position(0);
+        firstChildNode.setC_title("First Child Node");
+        firstChildNode.setC_parentid(1);
+        tree.add(firstChildNode);
+    }
+    
+    @Override
+    public <T extends ComprehensiveTree> List<T> getChildNode(T pNode) 
+            throws Exception {
+        
+        List<ComprehensiveTree> rChildNodes = new ArrayList<>();
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_parentid() == pNode.getC_id()) {
+                rChildNodes.add(node);
+            }
+        }
+        
+        // TODO ORDER BY C_POSITION
+        
+        return (List<T>) rChildNodes;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> List<T> searchNodeByString(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> List<String> searchNodeByPosition(
+            List<T> searchNodeByPositions) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> T getNode(T pNode)
+            throws Exception {
+        
+        ComprehensiveTree rNode = null;
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_id() == pNode.getC_id()) {
+                rNode = node;
+                break;
+            }
+        }
+        
+        return (T) rNode;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> T getNodeByRef(T pNode)
+            throws Exception {
+        
+        ComprehensiveTree rNode = null;
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_id() == pNode.getRef()) {
+                rNode = node;
+                break;
+            }
+        }
+        
+        return (T) rNode;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void cutMyself(
+            T p_OnlyCutMyselfFromJstree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void stretchPositionForMyselfFromJstree(T pNode) 
+            throws Exception {
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if ( node.getC_parentid() == pNode.getRef() 
+              && node.getC_position() >= pNode.getC_position() ) {
+              
+                node.setC_position(node.getC_position() + 1);
+            }
+        }
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void stretchLeftRightForMyselfFromJstree(T pNode) 
+            throws Exception {
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_left() >= pNode.getRightPositionFromNodeByRef()) {
+              
+                node.setC_left(node.getC_left() + pNode.getSpaceOfTargetNode());
+            }
+        }
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_right() >= pNode.getRightPositionFromNodeByRef()) {
+              
+                node.setC_right(node.getC_right() + pNode.getSpaceOfTargetNode());
+            }
+        }
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> int pasteMyselfFromJstree(
+            T p_OnlyPasteMyselfFromJstree) throws Exception {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> List<T> getChildNodeByLeftRight(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void fixCopy(T comprehensiveTree)
+            throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void fixCopyIF(T comprehensiveTree)
+            throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void enterMyselfFromJstree(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> int addMyselfFromJstree(T node) 
+            throws Exception {
+        
+        int id = generateId();
+        
+        node.setC_id(id);
+        
+        tree.add(node);
+        
+        return id;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> int alterNode(T pNode)
+            throws Exception {
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_id() == pNode.getC_id()) {
+                
+                node.setC_type(pNode.getC_type());
+                node.setC_title(pNode.getC_title());
+            }
+        }
+        
+        return 1;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> int removeNode(T comprehensiveTree)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> int alterNodeType(T comprehensiveTree)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void stretchLeftForMyselfFromJstree(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void stretchRightForMyselfFromJstree(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void enterMyselfFixPosition(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public <T extends ComprehensiveTree> void enterMyselfFixLeftRight(
+            T comprehensiveTree) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+    private int generateId() {
+        
+        int id = 0;
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if (node.getC_id() > id) {
+                
+                id = node.getC_id();
+            }
+        }
+        
+        return id + 1;
+    }
+    
+    public int generatePosition(ComprehensiveTree pNode) {
+        
+        int maxPosition = 0;
+        
+        for (ComprehensiveTree node : tree) {
+            
+            if ( node.getC_parentid() == pNode.getC_id() 
+              && node.getC_position() > maxPosition ) {
+                
+                maxPosition = node.getC_position();
+            }
+        }
+        
+        return maxPosition + 1;
+    }
+}
