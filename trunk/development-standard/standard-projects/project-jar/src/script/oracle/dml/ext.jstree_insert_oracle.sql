3f6c8c55082542a1ddaed40d4f9ce61b97235ca3
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-02-11 01:30:56 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-56 #resolve #time 3h #comment dml 세밀하게 조정. [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-jar/src/script/oracle/dml/ext.jstree_insert_oracle.sql
+++ trunk/development-standard/standard-projects/project-jar/src/script/oracle/dml/ext.jstree_insert_oracle.sql
@@ -15,11 +15,11 @@ Insert into T_COMPREHENSIVETREE
     C_LEVEL, C_TITLE, C_TYPE)
  Values
    (3, 2, 0, 3, 4, 
-    2, 'New node', 'default');
+    2, 'Leaf Node', 'default');
 Insert into T_COMPREHENSIVETREE
    (C_ID, C_PARENTID, C_POSITION, C_LEFT, C_RIGHT, 
     C_LEVEL, C_TITLE, C_TYPE)
  Values
    (4, 2, 1, 5, 6, 
-    2, 'New node', 'folder');
+    2, 'Branch Node', 'folder');
 COMMIT;
