6bc0ab06bf47fa870083053990940111435c4196
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-04-10 09:26:01 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-139 #resolve #time 3h #comment 크롤러 개발 ( ahnlab ) 을 여기서 개발함. -> CSV 파일 export 기능만 구현하면 끝. [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/scheduler/job/fecruToEazyBI/SvnDataImporter.java
+++ trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/scheduler/job/fecruToEazyBI/SvnDataImporter.java
@@ -1,27 +1,22 @@
 package egovframework.com.ext.jstree.support.manager.scheduler.job.fecruToEazyBI;
 
-import java.io.FileInputStream;
+
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.List;
-import java.util.zip.GZIPInputStream;
 
-import javax.servlet.http.Cookie;
-
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.InputSource;
-
-import com.sun.syndication.feed.synd.SyndEntry;
-import com.sun.syndication.feed.synd.SyndFeed;
-import com.sun.syndication.io.FeedException;
-import com.sun.syndication.io.SyndFeedInput;
 
 public class SvnDataImporter implements Job
 {
@@ -31,91 +26,91 @@ public class SvnDataImporter implements Job
     public void execute(JobExecutionContext arg0) throws JobExecutionException
     {
         
-        String url = "http://www.313.co.kr/fecru/rest-service-fe/search-v1/queryAsRows/standardSvn";
-        String query = "?query=";
-        query += "select revisions where date in [ 2014-12-10, 2014-12-12 ] "; 
-        query += "return path , dir , revision , author , date , comment , csid , isBinary , totalLines , ";
-        query += "linesAdded , linesRemoved , isAdded , isDeleted , isCopied , isMoved , tags , reviews";
         try
         {
-            
-            SyndFeed syndFeed = getSyndFeedForUrl(url + query);
-            System.out.println("RSS title: " + syndFeed.getTitle());
-            System.out.println("RSS author: " + syndFeed.getAuthor());
-
-            @SuppressWarnings("rawtypes")
-            List entries = syndFeed.getEntries();
-            for (int i = 0; i < entries.size(); i++) {
-                SyndEntry entry = (SyndEntry) entries.get(i);
-                System.out.println("--- Entry " + i);
-                System.out.println(entry.getTitle());
-                System.out.println(entry.getAuthor());
-                System.out.println(entry.getDescription().getValue());
-                System.out.println(entry.getLink());
+            String authUrlAddr = "http://www.313.co.kr/fecru/rest-service/auth-v1/login?userName=admin&password=love0618";
+            Element authRootNode = getRootNodeFromUrl(authUrlAddr);
+            Element authRootChildNode =authRootNode.getChild("token");
+            String authToken = authRootChildNode.getText();
+            if(authToken.isEmpty()){
+                logger.error("Fisheye error thorw : not auth");
+            }else{
+                //repository list getter;
+                //repositoryCheckOut(authToken);
+                logger.info("auth token check : " + authToken);
+                String queryUrl = "http://www.313.co.kr/fecru/rest-service-fe/repositories-v1";
+                String query = "?FEAUTH=" + authToken;
+                String url = queryUrl + query;
+                logger.info(url);
+                
+                Element root = getRootNodeFromUrl(url);
+                @SuppressWarnings("unchecked")
+                List<Element> repositorys =root.getChildren("repository");
+                for (Element repository : repositorys)
+                {
+                    logger.info(repository.getText() + repository.getAttributeValue("repositoryState"));
+                    logger.info(repository.getText() + repository.getAttributeValue("name"));
+                    logger.info(repository.getText() + repository.getAttributeValue("finishedFullSlurp"));
+                    logger.info(repository.getText() + repository.getAttributeValue("enabled"));
+                    repositoryCheckOut(authToken, repository.getAttributeValue("name"));
+                }
+                
             }
         }
-        catch (IllegalArgumentException | IOException | FeedException e)
+        catch (Exception e)
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-    
-    public SyndFeed getSyndFeedForUrl(String url) throws MalformedURLException, IOException, IllegalArgumentException,
-            FeedException
+
+    private void repositoryCheckOut(String authToken, String repositoryName) throws UnsupportedEncodingException, JDOMException, IOException,
+            MalformedURLException
     {
+        logger.info("auth token check : " + authToken);
         
-        SyndFeed feed = null;
-        InputStream is = null;
+        String queryUrl = "http://www.313.co.kr/fecru/rest-service-fe/search-v1/queryAsRows/" + repositoryName;
+        String query = "?query=";
+        query += URLEncoder.encode("select revisions where date in [ 2014-12-10, 2014-12-12 ] ", "UTF-8");
+        query += URLEncoder.encode("return path , dir , revision , author , date , comment , csid , isBinary , totalLines , ", "UTF-8");
+        query += URLEncoder.encode("linesAdded , linesRemoved , isAdded , isDeleted , isCopied , isMoved , tags , reviews", "UTF-8");
+        query += "&FEAUTH=" + authToken;
+        String url = queryUrl + query;
+        logger.info(url);
         
-        try
-        {
-            URLConnection openConnection = new URL(url).openConnection();
-            openConnection.setRequestProperty("Cookie","FEAUTH=admin:1:9368d6e5998c5f2019f1c08085fe7a0c6c53df6c");
-            openConnection.setRequestProperty("content-type", "application/x-www-form-urlencoded; charset=utf-8");
-            openConnection.setDoOutput(true);
-            openConnection.setUseCaches(false);
-            is = openConnection.getInputStream();
-            if ("gzip".equals(openConnection.getContentEncoding()))
+        Element rootNode = getRootNodeFromUrl(url);
+        
+        
+        @SuppressWarnings("unchecked")
+        List<Element> rowNodes =rootNode.getChildren("row");
+        if(null ==rowNodes){
+            logger.error("no search data !");
+        }
+        else{
+            Element headingsNode = rootNode.getChild("headings");
+            @SuppressWarnings("unchecked")
+            List<Element> headList = headingsNode.getChildren("heading");
+            for (Element rowNode : rowNodes)
             {
-                is = new GZIPInputStream(is);
+                @SuppressWarnings("unchecked")
+                List<Element> itemList=rowNode.getChildren("item");
+                for(int i=0;i<itemList.size(); i++){
+                    String heading = headList.get(i).getText();
+                    if(heading.equals("comment")){
+                        
+                    }
+                    Element person_E=itemList.get(i);
+                    logger.info(heading + " == " + person_E.getText());
+                }
             }
-            InputSource source = new InputSource(is);
-            SyndFeedInput input = new SyndFeedInput();
-            feed = input.build(source);
-            
         }
-        catch (Exception e)
-        {
-            logger.error("Exception occured when building the feed object out of the url", e);
-        }
-        finally
-        {
-            if (is != null) is.close();
-        }
-        
-        return feed;
     }
-    
-    public SyndFeed getSyndFeedFromLocalFile(String filePath) throws MalformedURLException, IOException,
-            IllegalArgumentException, FeedException
+
+    private Element getRootNodeFromUrl(String url) throws JDOMException, IOException, MalformedURLException
     {
-        
-        SyndFeed feed = null;
-        FileInputStream fis = null;
-        try
-        {
-            fis = new FileInputStream(filePath);
-            InputSource source = new InputSource(fis);
-            SyndFeedInput input = new SyndFeedInput();
-            feed = input.build(source);
-        }
-        finally
-        {
-            fis.close();
-        }
-        
-        return feed;
+        SAXBuilder builder = new SAXBuilder(); 
+        Document jdomdoc = builder.build(new URL(url));
+        Element root= jdomdoc.getRootElement();
+        return root;
     }
-    
 }
