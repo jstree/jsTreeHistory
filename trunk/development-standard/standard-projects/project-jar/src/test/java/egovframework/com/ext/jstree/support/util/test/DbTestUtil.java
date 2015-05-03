90f35fd2f7ed66db4476ecd6396f9400cc71ebcd
hyungchae <hyungchae@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-05-03 03:09:34 +0000

    데이터베이스 식별 로직 추가

--- trunk/development-standard/standard-projects/project-jar/src/test/java/egovframework/com/ext/jstree/support/util/test/DbTestUtil.java
+++ trunk/development-standard/standard-projects/project-jar/src/test/java/egovframework/com/ext/jstree/support/util/test/DbTestUtil.java
@@ -1,5 +1,7 @@
 package egovframework.com.ext.jstree.support.util.test;
 
+import static org.junit.Assert.fail;
+
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
@@ -7,19 +9,32 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 public class DbTestUtil
 {
-	public static void resetAutoIncrementColumns(ApplicationContext applicationContext, String sequenceName, int startNumber)
+	public static void resetAutoIncrementColumns(ApplicationContext applicationContext, String sequenceName, int startSeqNumber) throws Exception
 	{
-		
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(applicationContext.getBean("dataSource-oracle",DataSource.class));
-		
-		String dropSequenceSql = " DROP SEQUENCE %s";
-		String createSequenceSql = "CREATE SEQUENCE %s START WITH %d CACHE 20 ";
-		
-		String dropSql = String.format(dropSequenceSql, sequenceName);
-		String createSql = String.format(createSequenceSql, sequenceName, startNumber);
-		
-		jdbcTemplate.execute(dropSql);
-		jdbcTemplate.execute(createSql);
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(applicationContext.getBean("dataSource-oracle", DataSource.class));
 
+		String databaseProductName = jdbcTemplate.getDataSource().getConnection().getMetaData().getDatabaseProductName();
+
+		if ("Oracle".equals(databaseProductName) == true)
+		{
+			String sequenceNamePrefix = sequenceName.substring(0, 2);
+			
+			if("T_".equals(sequenceNamePrefix) == false)
+			{
+				final String dropSequenceSql = " DROP SEQUENCE %S";
+				final String createSequenceSql = "CREATE SEQUENCE %s START WITH %d MAXVALUE 999999999999999999999999999 MINVALUE 0 NOCYCLE CACHE 20 NOORDER ";
+				
+				jdbcTemplate.execute(String.format(dropSequenceSql, sequenceName));
+				jdbcTemplate.execute(String.format(createSequenceSql, sequenceName, startSeqNumber));
+			}
+			else
+			{
+				fail("테이블 명명규칙(T_로 시작)에 맞지 않는 테이블입니다.");
+			}
+		}
+		else
+		{
+			fail("오라클이 아닌 DBMS의 시퀀스 초기화를 구현하세요.");
+		}
 	}
 }
