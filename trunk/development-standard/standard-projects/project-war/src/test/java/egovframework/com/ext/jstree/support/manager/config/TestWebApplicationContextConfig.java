c1fecc42e975d57eceef5c9021c08e9fbcd48bda
cyanism <cyanism@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-05-15 15:39:44 +0000

    [PROJECT-JAR][1.0.0-SNAPSHOT][류강하] [BT] N/A war 프로젝트 테스트 환경을 만들기 위한 초석 [RV] +review SR @spear79

---
+++ trunk/development-standard/standard-projects/project-war/src/test/java/egovframework/com/ext/jstree/support/manager/config/TestWebApplicationContextConfig.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2008-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package egovframework.com.ext.jstree.support.manager.config;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.github.springtestdbunit.bean.DatabaseDataSourceConnectionFactoryBean;
+
+import egovframework.com.ext.jstree.support.manager.config.WebApplicationContextConfig;
+
+/**
+ * Modification Information
+ * 
+ * @author 류강하
+ * @since 2015. 4. 16.
+ * @version 1.0
+ * @see <pre>
+ * Class Name  : TestWebApplicationContextConfig.java
+ * Description : JsTree Spring+iBATIS 버젼의 JUnit4 + DbUnit 테스트를 위해 WebApplicationContextConfig를 확장한 클래스
+ * Infomation  : JsTree Spring+iBATIS 버젼의 JUnit4 + DbUnit 테스트를 위해 WebApplicationContextConfig를 확장한 클래스
+ * 
+ * << 개정이력(Modification Information) >>
+ * 
+ * 수정일                수정자                 수정내용
+ * -------       ------------   -----------------------
+ * 2015. 4. 16.  류강하                 최초 생성
+ * 
+ * Copyright (C) 2015 by 313 DeveloperGroup  All right reserved.
+ * </pre>
+ */
+@Configuration
+@PropertySource({ "classpath:/META-INF/egovframework/egovProps/globals.properties"
+                , "classpath:/META-INF/prop/globals.properties" })
+public class TestWebApplicationContextConfig extends WebApplicationContextConfig 
+{
+    @Resource(name = "dataSource-${Globals.DbType}")
+    private DataSource dataSource;
+    
+    private @Value("${Globals.UserName}") String shemaName;
+    
+	@Bean
+	public DatabaseDataSourceConnectionFactoryBean dbUnitDatabaseConnection() 
+	{
+		DatabaseDataSourceConnectionFactoryBean databaseDataSourceConnectionFactoryBean = new DatabaseDataSourceConnectionFactoryBean(dataSource);
+		databaseDataSourceConnectionFactoryBean.setSchema(shemaName);
+		return databaseDataSourceConnectionFactoryBean;
+	}
+}
