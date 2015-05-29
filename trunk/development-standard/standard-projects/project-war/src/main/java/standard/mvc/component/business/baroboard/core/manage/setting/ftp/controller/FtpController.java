a28659f680a83d5de371fb31e99528ea6ed1cb52
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-05-29 07:17:48 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][손호성] [BT] SP-105 #comment FTP 설정 vo validation test case 준비 [RV] +review SR @spear79

---
+++ trunk/development-standard/standard-projects/project-war/src/main/java/standard/mvc/component/business/baroboard/core/manage/setting/ftp/controller/FtpController.java
@@ -0,0 +1,58 @@
+package standard.mvc.component.business.baroboard.core.manage.setting.ftp.controller;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import standard.mvc.component.business.baroboard.core.manage.setting.ftp.vo.FtpVO;
+import egovframework.com.ext.jstree.springiBatis.core.service.CoreService;
+import egovframework.com.ext.jstree.support.manager.mvc.controller.GenericAbstractController;
+
+/**
+ * 
+ * Modification Information
+ * 
+ * @author 손호성
+ * @since 2015. 5. 29.
+ * @version 1.0
+ * @see <pre>
+ *  Class Name  : FtpController.java
+ *  Description : 바로보드-코어-고급설정-FTP Controller
+ *  Infomation  :
+ * 
+ *  << 개정이력(Modification Information) >>
+ * 
+ *  수정일              수정자                  수정내용
+ *  -------       ------------   -----------------------
+ *  2015. 5. 29.        손호성                  최초생성
+ * 
+ *  Copyright (C) 2015 by MarkAny  All right reserved.
+ * </pre>
+ */
+@Controller
+@RequestMapping(value = "/core/manage/setting/ftp")
+public class FtpController extends GenericAbstractController {
+
+    @Resource(name = "FtpService")
+    private CoreService ftpService;
+
+    @Override
+    public Map<String, Map<String, Object>> bindTypes() {
+        return null;
+    }
+
+    @RequestMapping(value = "/index.do", method = {RequestMethod.GET})
+    public String getNode(ModelMap modelMap, HttpServletRequest request) throws Exception {
+        FtpVO serverVO = new FtpVO();
+        serverVO.setC_id(4);
+        modelMap.addAttribute("ftp", ftpService.getNode(serverVO));
+        return "/jsp/baroboard/core/manage/setting/ftp/index";
+    }
+
+}
