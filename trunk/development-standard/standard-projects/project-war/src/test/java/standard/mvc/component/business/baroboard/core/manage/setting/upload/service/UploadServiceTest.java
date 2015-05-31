788850afe451cbef36ac7ef813b8bfaf1fb951e3
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-05-31 01:01:58 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][손호성] [BT] SP-105 #comment 파일업로드 설정 database test [RV] +review SR @spear79

---
+++ trunk/development-standard/standard-projects/project-war/src/test/java/standard/mvc/component/business/baroboard/core/manage/setting/upload/service/UploadServiceTest.java
@@ -0,0 +1,40 @@
+package standard.mvc.component.business.baroboard.core.manage.setting.upload.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * Modification Information
+ * 
+ * @author 손호성
+ * @since 2015. 5. 31.
+ * @version 1.0
+ * @see <pre>
+ * Class Name  : UploadServiceTest.java
+ * Description : 바로보드-코어-고급설정-서버 파일업로드 테스트 클래스
+ * Infomation  : 
+ * 
+ * << 개정이력(Modification Information) >>
+ * 
+ * 수정일               수정자                 수정내용
+ * -------       ------------   -----------------------
+ * 2015. 5. 31.         손호성                 최초 생성
+ * 
+ * Copyright (C) 2015 by 313 DeveloperGroup  All right reserved.
+ * </pre>
+ */
+public class UploadServiceTest {
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @After
+    public void tearDown() throws Exception {}
+
+    @Test
+    public void test() {
+    }
+
+}
