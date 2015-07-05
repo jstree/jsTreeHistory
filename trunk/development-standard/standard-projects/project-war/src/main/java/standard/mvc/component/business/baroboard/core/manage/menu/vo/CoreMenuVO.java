e64869e4a1c0a8a13732be24ba85de3f7323fd72
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-07-05 08:16:14 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][손호성] [BT] SP-105 #comment menu service test case 진행중 [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-war/src/main/java/standard/mvc/component/business/baroboard/core/manage/menu/vo/CoreMenuVO.java
+++ trunk/development-standard/standard-projects/project-war/src/main/java/standard/mvc/component/business/baroboard/core/manage/menu/vo/CoreMenuVO.java
@@ -26,6 +26,70 @@ import egovframework.com.ext.jstree.springiBatis.core.vo.ComprehensiveTree;
  */
 public class CoreMenuVO extends ComprehensiveTree {
 
+	private String menuId;
+	private String pageId;
+	private String authId;
+	private String imageDefault;
+	private String imageHover;
+	private String imageSelected;
+	private String homeFl;
+
+	public String getMenuId() {
+		return menuId;
+	}
+
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+
+	public String getPageId() {
+		return pageId;
+	}
+
+	public void setPageId(String pageId) {
+		this.pageId = pageId;
+	}
+
+	public String getAuthId() {
+		return authId;
+	}
+
+	public void setAuthId(String authId) {
+		this.authId = authId;
+	}
+
+	public String getImageDefault() {
+		return imageDefault;
+	}
+
+	public void setImageDefault(String imageDefault) {
+		this.imageDefault = imageDefault;
+	}
+
+	public String getImageHover() {
+		return imageHover;
+	}
+
+	public void setImageHover(String imageHover) {
+		this.imageHover = imageHover;
+	}
+
+	public String getImageSelected() {
+		return imageSelected;
+	}
+
+	public void setImageSelected(String imageSelected) {
+		this.imageSelected = imageSelected;
+	}
+
+	public String getHomeFl() {
+		return homeFl;
+	}
+
+	public void setHomeFl(String homeFl) {
+		this.homeFl = homeFl;
+	}
+
 	@Override
 	public String getSqlMapSelector() {
 		return "coreMenu";
