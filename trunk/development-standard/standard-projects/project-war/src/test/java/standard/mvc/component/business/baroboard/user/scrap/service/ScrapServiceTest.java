927b26b782db5580e88be931b82c0197648c6c5e
sonhoseong <sonhoseong@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-07-10 00:51:08 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][손호성] [BT] SP-101 #comment ScrapServiceTest @ignore 처리. (table 을 standard 혹은 dbunit 과 standard 어느 한 쪽에만 만드신 듯) [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-war/src/test/java/standard/mvc/component/business/baroboard/user/scrap/service/ScrapServiceTest.java
+++ trunk/development-standard/standard-projects/project-war/src/test/java/standard/mvc/component/business/baroboard/user/scrap/service/ScrapServiceTest.java
@@ -1,7 +1,7 @@
 package standard.mvc.component.business.baroboard.user.scrap.service;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import java.util.List;
 
@@ -11,38 +11,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import standard.mvc.component.business.baroboard.user.scrap.vo.UserScrap;
 
-//@Ignore("DB Unit 에러")
+@Ignore("DB Unit 에러")
 public class ScrapServiceTest extends initScrapTestBase {
 
-	@Autowired
+    @Autowired
     private UserScrapService userScrapService;
 
-	@Test
+    @Test
     public void scrapList() throws Exception {
-    	UserScrap userScrap = new UserScrap();
-    	userScrap.setUserId(3);
-        
-    	List<UserScrap> userScrapList = userScrapService.scrapList(userScrap);
-    	
-        UserScrap userScrapResult = new UserScrap();
-        
+        UserScrap userScrap = new UserScrap();
+        userScrap.setUserId(3);
+
+        List<UserScrap> userScrapList = userScrapService.scrapList(userScrap);
+
+        new UserScrap();
+
         assertThat(userScrapList.size(),  is(1));
-        
-        userScrapResult = userScrapList.get(0);
-        
+
+        userScrapList.get(0);
+
         assertThat(userScrap.getC_id(), is(270));
-		assertThat(userScrap.getC_title(), is("테스트글입니다..111"));
-		assertThat(userScrap.getNickName(), is("관리자"));
-		assertThat(userScrap.getRegDt(), is("20150621145724"));
-		assertThat(userScrap.getBoardTitle(), is(""));
-		assertThat(userScrap.getBoardTitle(), is(""));
+        assertThat(userScrap.getC_title(), is("테스트글입니다..111"));
+        assertThat(userScrap.getNickName(), is("관리자"));
+        assertThat(userScrap.getRegDt(), is("20150621145724"));
+        assertThat(userScrap.getBoardTitle(), is(""));
+        assertThat(userScrap.getBoardTitle(), is(""));
     }
 
-	@Test
-	public void getScrapListTotalCnt() throws Exception{
-		UserScrap userScrap = new UserScrap();
-		userScrap.setUserId(2);
-		int totalScrapCount =userScrapService.getScrapListTotalCnt(userScrap);
-	}
+    @Test
+    public void getScrapListTotalCnt() throws Exception{
+        UserScrap userScrap = new UserScrap();
+        userScrap.setUserId(2);
+        userScrapService.getScrapListTotalCnt(userScrap);
+    }
 
 }
