8b86e95e5e4134f6b466b0d8020ad22742dfbe14
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2015-12-01 19:10:34 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-15 #resolve #time 1h #comment db프로퍼티 수정함. [RV] +review SR @admin

--- trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/config/SpringBootapplication.java
+++ trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/config/SpringBootapplication.java
@@ -5,17 +5,13 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 //1. spring.pom 파일 spring-boot 활성화
 //2. 본 자바 파일 확장자 변경.
-@SpringBootApplication
-@EnableScheduling
-@EnableWebMvcSecurity
+//@SpringBootApplication
+//@EnableScheduling
+//@EnableWebMvcSecurity
 public class SpringBootapplication{
 
     private static Log logger = LogFactory.getLog(SpringBootapplication.class);
@@ -38,7 +34,7 @@ public class SpringBootapplication{
     }
 
     public static void main(String[] args) throws Exception { 
-            SpringApplication.run(SpringBootapplication.class, args);
+            //SpringApplication.run(SpringBootapplication.class, args);
     }
     
 }
