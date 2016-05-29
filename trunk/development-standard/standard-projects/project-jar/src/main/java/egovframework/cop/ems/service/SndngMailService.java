0c70956aa291952af8756ff9511176cc73b04980
hyungchae <hyungchae@ae997c0a-2d90-be44-948b-7632c96526aa>
2016-05-29 01:18:01 +0000

    [PROJECT-JAR][1.0.0-SNAPSHOT][김형채] [BT] SP-137 #comment 미사용 패키지 삭제 [RV] +review SR @spear79

--- trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/cop/ems/service/SndngMailService.java
+++
@@ -1,9 +0,0 @@
-package egovframework.cop.ems.service;
-
-import org.apache.commons.mail.EmailException;
-
-import egovframework.com.ext.jstree.support.manager.security.login.vo.UserInfo;
-
-public interface SndngMailService {
-	String sendEmail(UserInfo userInfo) throws EmailException;
-}
