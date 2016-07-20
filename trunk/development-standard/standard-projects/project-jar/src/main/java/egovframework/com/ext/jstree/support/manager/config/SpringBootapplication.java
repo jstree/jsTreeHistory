f757ab0c08b65b1d042c150c63766d9825daf135
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2016-07-20 01:31:11 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-15 #resolve #time 1h #comment 스케쥴러 설정 변경 [RV] +review SR @admin

--- trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/config/SpringBootapplication.java
+++ trunk/development-standard/standard-projects/project-jar/src/main/java/egovframework/com/ext/jstree/support/manager/config/SpringBootapplication.java
@@ -1,18 +1,25 @@
 package egovframework.com.ext.jstree.support.manager.config;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 //1. spring.pom 파일 spring-boot 활성화
 //2. 본 자바 파일 확장자 변경.
 //@SpringBootApplication
 //@EnableScheduling
 //@EnableWebMvcSecurity
-public class SpringBootapplication{
+//@Configuration
+public class SpringBootapplication implements SchedulingConfigurer {
 
     private static Log logger = LogFactory.getLog(SpringBootapplication.class);
     
@@ -31,6 +38,16 @@ public class SpringBootapplication{
                     }
 
             };
+    }
+    
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(destroyMethod="shutdown")
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(100);
     }
 
     public static void main(String[] args) throws Exception { 
