6d339ecd14726502f4561818b36f1d07c7cb26fe
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2016-10-11 07:03:46 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-64 #resolve #time 1h #comment 디펜던시 분리 [RV] +review SR @admin

--- trunk/development-project/project-standard/standard-server/src/main/java/egovframework/com/ext/jstree/strutsiBatis/core/service/S_AlterNode.java
+++ trunk/development-project/project-standard/standard-server/src/main/java/egovframework/com/ext/jstree/strutsiBatis/core/service/S_AlterNode.java
@@ -62,7 +62,7 @@ public class S_AlterNode implements I_S_AlterNode {
 		// TODO : 리퀘스트에 따라서 DB 설정파일을 불러와서 선택적 리턴을 해야 함.
 		String returnStr = "";
 		if (request.getRequestURI().equals(
-				"/egovframework/com/ext/jstree/strutsiBatis/alterNode.action")) {
+				"/com/ext/jstree/strutsiBatis/alterNode.action")) {
 			returnStr = "jstreeStrutsiBatis.alterNode";
 		} else {
 			logger.debug(request.getRequestURI());
