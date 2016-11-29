33da6e149397e7b9e7d626417a172198c01097ed
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2016-11-29 09:06:56 +0000

    [PROJECT-WAR][1.0.0-SNAPSHOT][이동민] [BT] SP-64 #resolve #time 1h #comment dwr 연동완료 [RV] +review SR @admin

--- trunk/development-project/project-standard/standard-server/src/main/webapp/html/egovframework/com/ext/jstree/dwr/res/index.js
+++ trunk/development-project/project-standard/standard-server/src/main/webapp/html/egovframework/com/ext/jstree/dwr/res/index.js
@@ -10,320 +10,307 @@ var viewport = null;
 var loginWindow = null;
 
 function updateOnlineList(onlineList) {
-	if (onlineList && onlineList.length > 0) {
-		onlineListStore.loadData(onlineList);
-	}
+  console.log("updateOnlineList Call");
+  if (onlineList && onlineList.length > 0) {
+    onlineListStore.loadData(onlineList);
+  }
 };
 
 function addMessage(name, time, message) {
-	if (messageStore) {
-		messageStore.add({
-			name : name,
-			time : time,
-			message : message
-		});
-	}
+  if (messageStore) {
+    messageStore.add({
+      name: name,
+      time: time,
+      message: message
+    });
+  }
 
-	if (messageGrid) {
-		messageGrid.getView().focusRow(messageGrid.getStore().getCount() - 1);
-	}
+  if (messageGrid) {
+    messageGrid.getView().focusRow(messageGrid.getStore().getCount() - 1);
+  }
 }
 
 function showErrorBox(title, message, target) {
-	if (target) {
-		Ext.MessageBox.show({
-			title : title,
-			msg : message,
-			buttons : Ext.MessageBox.OK,
-			icon : Ext.MessageBox.ERROR,
-			animateTarget : target
-		});
-		setTimeout(function() {
-			Ext.MessageBox.hide();
-		}, 1500);
-	} else {
-		Ext.MessageBox.show({
-			title : title,
-			msg : msg,
-			width : 400,
-			icon : Ext.MessageBox.ERROR
-		});
-	}
+  if (target) {
+    Ext.MessageBox.show({
+      title: title,
+      msg: message,
+      buttons: Ext.MessageBox.OK,
+      icon: Ext.MessageBox.ERROR,
+      animateTarget: target
+    });
+    setTimeout(function() {
+      Ext.MessageBox.hide();
+    }, 1500);
+  } else {
+    Ext.MessageBox.show({
+      title: title,
+      msg: msg,
+      width: 400,
+      icon: Ext.MessageBox.ERROR
+    });
+  }
 }
 
 var page = {
-	username : null,
-	sendMessage : function() {
-		if (editor) {
-			editor.setLoading(true);
-			Chat.sendMessage(editor.getValue(), function(data) {
-				editor.setLoading(false);
-				if (SUCCESS == data) {
-					editor.setValue('');
-				} else {
-					showErrorBox('Error', 'Not Connect', editor);
-				}
-			});
-		}
-	},
+  username: null,
+  sendMessage: function() {
+    if (editor) {
+      editor.setLoading(true);
+      Chat.sendMessage(editor.getValue(), function(data) {
+        editor.setLoading(false);
+        if (SUCCESS == data) {
+          editor.setValue('');
+        } else {
+          showErrorBox('Error', 'Not Connect', editor);
+        }
+      });
+    }
+  },
 
-	openLoginWindow : function() {
-		if (loginWindow && viewport) {
-			loginWindow.show();
-			this.toggleDisabled();
-		}
-	},
+  openLoginWindow: function() {
+    if (loginWindow && viewport) {
+      loginWindow.show();
+      this.toggleDisabled();
+    }
+  },
 
-	login : function(form) {
-		loginWindow.setLoading(true);
-		form = form.up('form').getForm();
-		var name;
-		if (form.isValid()) {
-			name = form.getValues().username;
-		} else {
-			return;
-		}
-		Chat.login(name, function(data) {
-			loginWindow.setLoading(false);
-			if (SUCCESS == data) {
-				page.username = name;
-				page.toggleDisabled();
-				loginWindow.hide();
-			} else {
-				showErrorBox('Error', 'Not Connect', loginWindow);
-			}
-		});
-	},
+  login: function(form) {
+    loginWindow.setLoading(true);
+    form = form.up('form').getForm();
+    var name;
+    if (form.isValid()) {
+      name = form.getValues().username;
+    } else {
+      return;
+    }
+    Chat.login(name, function(data) {
+      loginWindow.setLoading(false);
+      if (SUCCESS == data) {
+        page.username = name;
+        page.toggleDisabled();
+        loginWindow.hide();
+      } else {
+        showErrorBox('Error', 'Not Connect', loginWindow);
+      }
+    });
+  },
 
-	disabled : false,
+  disabled: false,
 
-	toggleDisabled : function() {
-		if (this.disabled) {
-			this.disabled = false;
-			onlineListGrid.enable();
-			messageGrid.enable();
-			sendBox.enable();
-		} else {
-			this.disabled = true;
-			onlineListGrid.disable();
-			messageGrid.disable();
-			sendBox.disable();
-		}
-	}
+  toggleDisabled: function() {
+    if (this.disabled) {
+      this.disabled = false;
+      onlineListGrid.enable();
+      messageGrid.enable();
+      sendBox.enable();
+    } else {
+      this.disabled = true;
+      onlineListGrid.disable();
+      messageGrid.disable();
+      sendBox.disable();
+    }
+  }
 };
 
 Ext.tip.QuickTipManager.init();
 
 Ext.onReady(function() {
 
-	Ext.form.HtmlEditor.override({
-		frame : true,
-		initComponent : function() {
-			this.callOverridden();
-			this.addEvents('submit');
-		},
+  Ext.form.HtmlEditor.override({
+    frame: true,
+    initComponent: function() {
+      this.callOverridden();
+      this.addEvents('submit');
+    },
 
-		initEditor : function() {
-			this.callOverridden();
+    initEditor: function() {
+      this.callOverridden();
 
-			var me = this;
-			var doc = me.getDoc();
+      var me = this;
+      var doc = me.getDoc();
 
-			if (Ext.isGecko) {
-				Ext.EventManager.on(doc, 'keypress', me.fireSubmit, me);
-			}
+      if (Ext.isGecko) {
+        Ext.EventManager.on(doc, 'keypress', me.fireSubmit, me);
+      }
 
-			if (Ext.isIE || Ext.isWebKit || Ext.isOpera) {
-				Ext.EventManager.on(doc, 'keydown', me.fireSubmit, me);
-			}
-		},
+      if (Ext.isIE || Ext.isWebKit || Ext.isOpera) {
+        Ext.EventManager.on(doc, 'keydown', me.fireSubmit, me);
+      }
+    },
 
-		fireSubmit : function(e) {
-			if (e.ctrlKey && e.ENTER == e.getKey()) {
-				page.sendMessage();
-			}
-		}
-	});
+    fireSubmit: function(e) {
+      if (e.ctrlKey && e.ENTER == e.getKey()) {
+        page.sendMessage();
+      }
+    }
+  });
 
-	onlineListStore = Ext.create('Ext.data.Store', {
-		autoLoad : false,
-		fields : [ {
-			name : 'name',
-			type : 'string'
-		}, {
-			name : 'time',
-			type : 'string'
-		} ],
-		proxy : {
-			type : 'memory',
-			reader : {
-				type : 'array'
-			}
-		}
-	});
+  onlineListStore = Ext.create('Ext.data.Store', {
 
-	messageStore = Ext.create('Ext.data.Store', {
-		fields : [ {
-			name : 'name',
-			type : 'string'
-		}, {
-			name : 'time',
-			type : 'string'
-		}, {
-			name : 'message',
-			type : 'string'
-		} ],
-		proxy : {
-			type : 'memory',
-			reader : {
-				type : 'array'
-			}
-		}
-	});
+    fields: [{
+      name: 'name',
+      type: 'string'
+    }, {
+      name: 'time',
+      type: 'string'
+    }],
+    proxy: {
+      type: 'memory',
+      reader: {
+        type: 'array'
+      }
+    }
+  });
 
-	editor = Ext.create('Ext.form.HtmlEditor', {
-		fontFamilies : [ "Arial", "Courier New", "Tahoma", "Times New Roman",
-				"Verdana" ],
-		listeners : {
-			render : function() {
-				this.textareaEl.on('keydown', function() {
-					alert();
-				}, this);
-			}
-		}
-	});
+  messageStore = Ext.create('Ext.data.Store', {
+    fields: [{
+      name: 'name',
+      type: 'string'
+    }, {
+      name: 'time',
+      type: 'string'
+    }, {
+      name: 'message',
+      type: 'string'
+    }],
+    proxy: {
+      type: 'memory',
+      reader: {
+        type: 'array'
+      }
+    }
+  });
 
-	onlineListGrid = Ext.create('Ext.grid.Panel', {
-		region : 'east',
-		collapsible : true,
-		split : true,
-		width : 250,
-		title : 'onlineListGrid',
-		store : onlineListStore,
-		columns : [ {
-			text : 'name',
-			dataIndex : 'name',
-			flex : 1,
-			hideable : false
-		}, {
-			text : 'time',
-			dataIndex : 'time',
-			width : 138,
-			hidden : true
-		} ]
-	});
+  editor = Ext.create('Ext.form.HtmlEditor', {
+    fontFamilies: ["Arial", "Courier New", "Tahoma", "Times New Roman", "Verdana"],
+    listeners: {
+      render: function() {
+        this.textareaEl.on('keydown', function() {
+          alert();
+        }, this);
+      }
+    }
+  });
 
-	messageGrid = Ext.create('Ext.grid.Panel', {
-		region : 'center',
-		rowLines : false,
-		store : messageStore,
-		columns : [ {
-			text : 'message',
-			dataIndex : 'message',
-			flex : 1,
-			hideable : false,
-			renderer : function(value, p, record) {
-				return Ext.String.format(
-						'<p class="message-meta"><strong>{0}</strong> <em>{1}</em></p><p>{2}</p>',
-						record.data.name, record.data.time, value);
-			}
-		} ]
-	});
+  onlineListGrid = Ext.create('Ext.grid.Panel', {
+    region: 'east',
+    collapsible: true,
+    split: true,
+    width: 250,
+    title: 'onlineListGrid',
+    store: onlineListStore,
+    columns: [{
+      text: 'name',
+      dataIndex: 'name',
+      flex: 1,
+      hideable: false
+    }, {
+      text: 'time',
+      dataIndex: 'time',
+      width: 138,
+      hidden: false
+    }]
+  });
 
-	sendBox = Ext.create('Ext.panel.Panel', {
-		region : 'south',
-		collapsible : true,
-		split : true,
-		height : 200,
-		weight : -100,
-		layout : 'fit',
-		title : '입력창',
-		items : [ editor ],
-		bbar : [ '->', {
-			width : 160,
-			text : '입력 (Ctrl + Enter)',
-			handler : function() {
-			  page.sendMessage();
-			}
-		} ]
-	});
+  messageGrid = Ext.create('Ext.grid.Panel', {
+    region: 'center',
+    rowLines: false,
+    store: messageStore,
+    columns: [{
+      text: 'message',
+      dataIndex: 'message',
+      flex: 1,
+      hideable: false,
+      renderer: function(value, p, record) {
+        return Ext.String.format('<p class="message-meta"><strong>{0}</strong> <em>{1}</em></p><p>{2}</p>', record.data.name, record.data.time, value);
+      }
+    }]
+  });
 
-	var header = {
-		xtype : 'box',
-		region : 'north',
-		html : '<h1>header</h1>',
-		cls : 'header',
-		height : 50
-	};
+  sendBox = Ext.create('Ext.panel.Panel', {
+    region: 'south',
+    collapsible: true,
+    split: true,
+    height: 200,
+    weight: -100,
+    layout: 'fit',
+    title: '입력창',
+    items: [editor],
+    bbar: ['->', {
+      width: 160,
+      text: '입력 (Ctrl + Enter)',
+      handler: function() {
+        page.sendMessage();
+      }
+    }]
+  });
 
-	viewport = Ext.create('Ext.Viewport', {
-		layout : {
-			type : 'border'
-		},
-		defaults : {
-			split : false
-		},
-		items : [ header, onlineListGrid, messageGrid, sendBox ]
-	});
+  viewport = Ext.create('Ext.Viewport', {
+    layout: {
+      type: 'border'
+    },
+    defaults: {
+      split: false
+    },
+    items: [onlineListGrid, messageGrid, sendBox]
+  });
 
-	var required = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
+  var required = '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>';
 
-	loginWindow = Ext.create('Ext.Window', {
-		title : 'simpleForm',
-		width : 300,
-		height : 94,
-		closable : false,
-		border : false,
-		items : [ {
-			xtype : 'form',
-			layout : 'form',
-			id : 'simpleForm',
-			url : 'save-form.php',
-			frame : true,
-			fieldDefaults : {
-				msgTarget : 'side',
-				labelWidth : 75
-			},
-			defaultType : 'textfield',
-			items : [ {
-				fieldLabel : 'username',
-				name : 'username',
-				afterLabelTextTpl : required,
-				allowBlank : false,
-				listeners : {
-					specialkey : function(field, e) {
-						// e.HOME, e.END, e.PAGE_UP, e.PAGE_DOWN,
-						// e.TAB, e.ESC, arrow keys: e.LEFT, e.RIGHT, e.UP,
-						// e.DOWN
-						if (e.getKey() == e.ENTER) {
-							page.login(this);
-						}
-					}
-				}
-			} ],
-			buttons : [ {
-				text : 'form',
-				handler : function() {
-					this.up('form').getForm().reset();
-				}
-			}, {
-				text : '로그인',
-				handler : function() {
-					page.login(this);
-				}
-			} ]
-		} ]
-	});
+  loginWindow = Ext.create('Ext.Window', {
+    title: 'simpleForm',
+    width: 300,
+    height: 94,
+    closable: false,
+    border: false,
+    items: [{
+      xtype: 'form',
+      layout: 'form',
+      id: 'simpleForm',
+      url: 'save-form.php',
+      frame: true,
+      fieldDefaults: {
+        msgTarget: 'side',
+        labelWidth: 75
+      },
+      defaultType: 'textfield',
+      items: [{
+        fieldLabel: 'username',
+        name: 'username',
+        afterLabelTextTpl: required,
+        allowBlank: false,
+        listeners: {
+          specialkey: function(field, e) {
+            if (e.getKey() == e.ENTER) {
+              page.login(this);
+            }
+          }
+        }
+      }],
+      buttons: [{
+        text: 'form',
+        handler: function() {
+          this.up('form').getForm().reset();
+        }
+      }, {
+        text: '로그인',
+        handler: function() {
+          page.login(this);
+        }
+      }]
+    }]
+  });
 
-	(function() {
-		if (!page.username) {
-			page.openLoginWindow();
-		}
-	})();
+  (function() {
+    if (!page.username) {
+      page.openLoginWindow();
+    }
+  })();
 
-	dwr.engine.setActiveReverseAjax(true);
-	dwr.engine.setNotifyServerOnPageUnload(true);
-	dwr.engine.setErrorHandler(function() {
-	  alert('DWR Error');
-	});
+  dwr.engine.setActiveReverseAjax(true);
+  dwr.engine.setNotifyServerOnPageUnload(true);
+  dwr.engine.setErrorHandler(function() {
+    alert('DWR Error');
+  });
 });
