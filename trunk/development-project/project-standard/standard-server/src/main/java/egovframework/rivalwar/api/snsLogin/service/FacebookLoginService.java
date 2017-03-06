a66bbf5b9fdb0a6e49832eed590a826483142ff6
admin <admin@ae997c0a-2d90-be44-948b-7632c96526aa>
2017-03-06 08:55:14 +0000

    [BT] SP-359 #resolve #time 1h #comment 페이스북 최종 완료 [RV] +review SR @admin

--- trunk/development-project/project-standard/standard-server/src/main/java/egovframework/rivalwar/api/snsLogin/service/FacebookLoginService.java
+++ trunk/development-project/project-standard/standard-server/src/main/java/egovframework/rivalwar/api/snsLogin/service/FacebookLoginService.java
@@ -1,9 +1,10 @@
 package egovframework.rivalwar.api.snsLogin.service;
 
+import egovframework.com.sec.rgm.service.AuthorGroup;
 import org.springframework.social.facebook.api.FacebookProfile;
 
 public interface FacebookLoginService {
 
-    public String getUserIdByLoginAndRegisterProcess(FacebookProfile facebookProfile) throws Exception;
+    public String getUserIdByLoginAndRegisterProcess(FacebookProfile facebookProfile, AuthorGroup authorGroup) throws Exception;
 
 }
